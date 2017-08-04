ade81597df4a9734ac641a2f40e6022c7ebe90aa
Michael Robbeloth <michael@robbeloth.com>
2017-08-04 16:11:39 -0400

    Saving and restoring previous checkins from senior relative

--- app/src/main/java/robbeloth/consulting/services/stillvertical/NotifyRelativeActivity.java
+++ app/src/main/java/robbeloth/consulting/services/stillvertical/NotifyRelativeActivity.java
@@ -3,6 +3,7 @@ package robbeloth.consulting.services.stillvertical;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import android.os.PersistableBundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +18,9 @@ import java.util.Date;
 
 public class NotifyRelativeActivity extends AppCompatActivity {
 
+    private static final String TAG = "NRActivity";
+    private static final String CHECKIN_KEY = "CHECKIN";
+
     private Button mIAmOkayButton;
     private Button mPrevButton;
     private Button mNextButton;
@@ -30,22 +34,61 @@ public class NotifyRelativeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notify_relative);
 
+        if (savedInstanceState != null) {
+            for (int i = 0; i < savedInstanceState.size(); i++) {
+                if (savedInstanceState.get(CHECKIN_KEY+i) != null) {
+                    Object o = savedInstanceState.get(CHECKIN_KEY+i);
+                    if (o instanceof Long) {
+                        Notification n = new Notification(new Date((long)o));
+                        mNotificationArrayList.add(n);
+                    }
+                }
+            }
+        }
+
         // wire up the widgets for use, don't let prev next buttons get used with no checkins
         mIAmOkayButton = (Button) findViewById(R.id.aokay_button);
         mPrevButton = (Button) findViewById(R.id.prev_button);
-        mPrevButton.setEnabled(false);
         mNextButton = (Button) findViewById(R.id.next_button);
-        mNextButton.setEnabled(false);
         mPrevCheckins = (TextView) findViewById(R.id.notication_time);
-
         mLastTimeRelativeWasOkay = (Chronometer) findViewById(R.id.time_since_last_notification);
+        if (mNotificationArrayList.size() > 0) {
+            // Show most recent checkin
+            Notification n = mNotificationArrayList.get(mNotificationArrayList.size()-1);
+            Date d = n.getEntry();
+            mPrevCheckins.setText(d.toString());
+
+            /* Show time since most recent checkin
+            *  currentTimeMillis is ms since Jan 1 1970
+            *  elapsedRealtime is ms since boot
+            *
+            *  1. Subtract two points in time from Jan 1 1970 to get
+            *  ms between those two points in time
+            *  2. Subtract the difference from the current number of
+            *  ms since boot to get a basis that is greater than zero
+            *  or the last key press
+            *
+            *  yep, it's messy and not intuitive for a chronometer */
+            long curTime = System.currentTimeMillis();
+            long msSinceBoot = SystemClock.elapsedRealtime();
+            long mostRecentTime = d.getTime();
+            long basis = msSinceBoot - (curTime - mostRecentTime);
+            mLastTimeRelativeWasOkay.stop();
+            mLastTimeRelativeWasOkay.setBase(basis);
+            mLastTimeRelativeWasOkay.start();
+        }
+        else {
+            mPrevButton.setEnabled(false);
+            mNextButton.setEnabled(false);
+        }
+
 
         // Provide callback ref as anonymous inner class
         mIAmOkayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Log.d("NRActivity", "Relative clicked I am okay");
+                Log.d(TAG, "Relative clicked I am okay");
 
                 // Store time of checkin by elderly relative
                 Calendar c = Calendar.getInstance();
@@ -110,5 +153,21 @@ public class NotifyRelativeActivity extends AppCompatActivity {
                 mPrevCheckins.setText(d.toString());
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.d(TAG, "onSaveInstanceState() -- Saving data");
+
+        // Convert the dates to longs (millisecond rep)
+        int cnt = 0;
+        for (Notification checkin : mNotificationArrayList) {
+            Date d = checkin.getEntry();
+            outState.putLong(CHECKIN_KEY+cnt,d.getTime());
+            cnt++;
+        }
+
     }
 }
